d2e48c72bda04071982da1dc51463a5ebfad3ee1
Dr. Alsayed <Dr. Alsayed@DESKTOP-APIOASQ>
2025-07-29 21:02:07 +0300

    Add project files.

---
+++ Lab 9/Lab 9.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int num1, num2;
+
+	cout << "Please, enter the first num: ";
+	cin >> num1;
+
+	cout << "Please, enter the second num: ";
+	cin >> num2;
+
+	if (num1 > num2) {
+		cout << "The first num is greater than the second num!" << "\n";
+
+	}
+
+	else {
+		cout << "The second num is greater than or equal to the first num!" << "\n";
+
+	}
+
+
+
+	return 0;
+}
+
