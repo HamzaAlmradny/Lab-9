a792d4be9521f7c6f671f8a2f883324279108015
HamzaAlmradny <h.almradny@gmail.com>
2025-08-01 18:08:54 +0300

    Lab 9

--- Lab 9/Lab 9.cpp
+++ Lab 9/Lab 9.cpp
@@ -1,4 +1,18 @@
 
+// Documentation Section 
+
+/*
+File Name : Lab 9
+Author : Hamza Alsayed Almradny
+Version : V.1
+Purpose : Ask the user to enter 2 numbers and print the largest one of the two numbers.
+Date : 17/7/2025
+*/
+
+
+
+
+
 #include <iostream>
 using namespace std;
 
